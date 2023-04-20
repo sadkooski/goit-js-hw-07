2f68dd6e015f2525521bf170af3f5f4c2a783cdc
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-21 00:03:56 +0200

    1: done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-console.log(galleryItems);
-
-// gallery.addEventListener("click", modalImage);
-
-// function modalImage()
+// console.log(galleryItems);
 
 function createImagesGallery() {
-  // for (let i = 0; i < galleryItems.length - 1; i++)
   gallery.classList.add("gallery");
-  console.log(gallery.classList);
+  // console.log(gallery.classList);
+
   for (const item of galleryItems) {
     const htmlImageString = `<li class="gallery__item"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></li>`;
     gallery.innerHTML += htmlImageString;
@@ -18,3 +14,23 @@ function createImagesGallery() {
 }
 
 createImagesGallery();
+
+function selectedImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(event.target.alt);
+
+  for (const item of galleryItems) {
+    if (event.target.alt === item.description) {
+      const instance = basicLightbox.create(`
+    <img src="${item.original}">`);
+      instance.show();
+      console.log(item.description);
+    }
+  }
+}
+
+gallery.addEventListener("click", selectedImage);
+
+console.log(basicLightbox);
