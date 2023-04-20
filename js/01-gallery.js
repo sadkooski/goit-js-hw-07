94dacffcdbec44ab4b4d120f4d0c18444b1aac39
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-20 15:44:48 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
+
+// gallery.addEventListener("click", modalImage);
+
+// function modalImage()
+
+function createImagesGallery() {
+  // for (let i = 0; i < galleryItems.length - 1; i++)
+  gallery.classList.add("gallery");
+  console.log(gallery.classList);
+  for (const item of galleryItems) {
+    const htmlImageString = `<li class="gallery__item"><img class="gallery__image" src="${item.preview}" alt="${item.description}"></li>`;
+    gallery.innerHTML += htmlImageString;
+  }
+}
+
+createImagesGallery();
