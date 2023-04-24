7d4a005ee1d7d6d2bd2865189f065540e69a081b
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-04-25 00:24:07 +0200

    1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,18 +2,18 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 // console.log(galleryItems);
+const modal = document.querySelector(".basicLightbox");
 
 function createImagesGallery() {
   gallery.classList.add("gallery");
   // console.log(gallery.classList);
-
   for (const item of galleryItems) {
     const htmlImageString = `<li class="gallery__item">
      <a class="gallery__link" href="${item.original}">
         <img 
         class="gallery__image" 
         src="${item.preview}" 
-        dat5a-source="${item.original}" 
+        data-source="${item.original}"  
         alt="${item.description}" />
      </a>
    </li>`;
@@ -24,6 +24,7 @@ function createImagesGallery() {
 createImagesGallery();
 
 function selectedImage(event) {
+  event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
@@ -32,9 +33,17 @@ function selectedImage(event) {
   for (const item of galleryItems) {
     if (event.target.alt === item.description) {
       const instance = basicLightbox.create(`
-    <img src="${item.original}">`);
+      <img src="${item.original}">`);
       instance.show();
       console.log(item.description);
+
+      document.addEventListener("keydown", (event) => {
+        if (event.code !== "Escape") {
+          return;
+        }
+        instance.close();
+        console.log(event.code);
+      });
     }
   }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+// console.log(galleryItems);
 
-console.log(galleryItems);
+function createImagesGallery() {
+  gallery.classList.add("gallery");
+  // console.log(gallery.classList);
+  for (const item of galleryItems) {
+    const htmlImageString = `<li class="gallery__link">
+     <a class="gallery__item" href="${item.original}">
+        <img 
+        class="gallery__image" 
+        src="${item.preview}" 
+        data-source="${item.original}"  
+
+        alt="${item.description}" />
+     </a>
+   </li>`;
+    gallery.innerHTML += htmlImageString;
+  }
+}
+
+createImagesGallery();
+
+function selectedImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(event.target.alt);
+
+  for (const item of galleryItems) {
+    if (event.target.alt === item.description) {
+      let lightbox = simpleLightbox.create(`<img src="${item.original}">`);
+      lightbox.show();
+      //   const instance = simpleLightbox.create(`
+      // `);
+
+      console.log(item.description);
+    }
+  }
+}
+
+gallery.addEventListener("click", selectedImage);
+
+console.log(simpleLightbox);
